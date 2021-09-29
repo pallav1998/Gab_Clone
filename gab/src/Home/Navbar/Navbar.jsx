2a6992d2375450b0be3c1b66685c7952080ea3b7
Milind <milindanand74@gmail.com>
2021-09-29 15:59:18 +0530

    navbar done

--- gab/src/Home/Navbar/Navbar.jsx
+++ gab/src/Home/Navbar/Navbar.jsx
@@ -12,49 +12,82 @@ import MessageIcon from '@mui/icons-material/Message';
 import LightbulbRoundedIcon from '@mui/icons-material/LightbulbRounded';
 import PersonIcon from '@mui/icons-material/Person';
 
+import SearchIcon from '@mui/icons-material/Search';
+import Paper from '@mui/material/Paper';
+
+
+
 
 
 
 const Navbar = () => {
     return (
+      <>
+
+      <Paper elevation={1} >
+
         <div className={styles.Container}>
             <div className={styles.logo}>
-                <img height="70px" src="https://apps.gab.com/img/gab-logo.png" alt="" />
+                <img style={{marginTop:"-7px"}} height="58px" src="https://apps.gab.com/img/gab-logo.png" alt="" />
                   </div>
             <div className={styles.search}>
+<div className={styles.searchinput}>
+
             <input type="text" placeholder=    "Search for people or group on Gab" />
+           <div style={{float:"right",marginTop:"7px",marginRight:"15px"}}>
+           <SearchIcon style={{color:" 63da9d"}} />
+
+           </div>
+
+            </div>
             </div>
 
 
             <div className={styles.icons}>
 <div>
-
-            <HomeIcon   style={{ color:" #63da9d" }} />
-            <div>Home</div>
+<IconButton>
+            <HomeIcon style={{color:"#2d3436"}}   />
+            </IconButton>
+            <div  className={styles.navbartext}>Home</div>
             </div>
             <div>
-                <LanguageIcon   style={{ color:"rgb(202, 194, 194);" }} />
-             <div>Explore</div>
+            <IconButton>
+
+                <LanguageIcon    style={{color:"#2d3436" }} />
+                </IconButton>
+
+             <div  className={styles.navbartext}>Explore</div>
          
            </div>
 
 <div>
-      <DescriptionIcon  style={{ color:"rgb(202, 194, 194);" }} />
-         <div>News</div>
+<IconButton>
+      <DescriptionIcon  style={{ color:"#2d3436" }} />
+      </IconButton>
+         <div  className={styles.navbartext}>News</div>
          
 </div>
-<div>
-      <GroupsIcon   style={{ color:"rgb(202, 194, 194);",fontSize:"30px","marginTop":"-4px"}} />
-         <div>Groups</div>
-         
+<div >
+    <IconButton>
+      <GroupsIcon   style={{ color:"#2d3436",fontSize:"30px","marginTop":"-4px"}} />
+      </IconButton>
+
+         <div className={styles.navbartext}>Groups</div>
+
+
 </div>
 
 <div>
+<IconButton>
+
       <LiveTvIcon              
-   style={{ color:"rgb(202, 194, 194);" }} />
-         <div>TV</div>
+   style={{  color:"#2d3436" }} />
+   </IconButton>
+
+         <div  className={styles.navbartext}>TV</div>
          
 </div>
+
 
 <div>
       <p className={styles.samecolor}>|</p>
@@ -75,9 +108,12 @@ const Navbar = () => {
 </div>
 <div className={styles.samecolor1}>
 <IconButton  style={{border:"50%",background:"rgb(240,240,240)"}}
-size="medium">
+size="medium"
+  aria-label="show 6 new messages">
+              <Badge badgeContent={5} color="error">
 
 <MessageIcon/>
+</Badge>
              </IconButton>
 
 
@@ -112,10 +148,14 @@ size="medium">
 
 </div>
 
+</Paper>
 
+<div className={styles.container2}>
 
+</div>
        
 
+       </>
         
 
 
