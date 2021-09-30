34ffa9ecdd22a647c5d6eeabdc52f8b1289b9a7e
Milind <milindanand74@gmail.com>
2021-09-30 21:31:32 +0530

    story section part1 done

---
+++ Frontend/src/Home/LandingPage/FirstPost.jsx
@@ -0,0 +1,60 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import styles from "../LandingPage/Firstpost.module.css"
+import CreateIcon from '@mui/icons-material/Create';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+export default function MediaCard() {
+  return (
+    <div className={styles.cardparent} >
+
+    <Card className={styles.card1} >
+      <div className={styles.loadcard1}>
+      {/* <i style={{fontSize:"70px",position:"relative"}}  class="fas fa-spinner">          */}
+{/* </i> */}
+<CreateIcon className={styles.card1icon}  />
+
+      </div>
+      <CardContent>
+        <Typography   style={{fontSize:"15px",color:"#2d3436"}} gutterBottom variant="h5" component="div">
+          Send your first post
+        </Typography>
+       
+      </CardContent>
+    </Card>
+
+    <Card className={styles.card1} >
+      <div className={styles.loadcard1}>
+      {/* <i style={{fontSize:"70px"}}  class="fas fa-spinner">         
+{/* </i> */}
+      <i style={{color:"black",fontSize:"20px",marginTop:"30px"}}class="fas fa-users"></i> 
+
+      </div>
+      <CardContent>
+        <Typography style={{fontSize:"15px",color:"#2d3436"}} gutterBottom variant="h5" component="div">
+        Follow Others
+        </Typography>
+       
+      </CardContent>
+    </Card>
+
+    <Card className={styles.card1} >
+      <div className={styles.loadcard1}>
+      {/* <i style={{fontSize:"70px"}}  class="fas fa-spinner">          */}
+      < InsertPhotoIcon style={{color:"black",marginTop:"32px",fontSize:"30px"}} />
+
+{/* </i> */}
+      </div>
+      <CardContent>
+        <Typography style={{fontSize:"15px",color:"#2d3436"}} gutterBottom variant="h5" component="div">
+Complete your profile
+        </Typography>
+       
+      </CardContent>
+    </Card>
+
+    
+    </div>
+
+  );
+}
--- Frontend/src/Home/LandingPage/Postpage.jsx
+++ Frontend/src/Home/LandingPage/Postpage.jsx
@@ -1,94 +1,120 @@
-import React from 'react'
-import styles from "../Navbar/navbar.module.css"
-import IconButton from '@mui/material/IconButton';
-import PersonIcon from '@mui/icons-material/Person';
+import React,{useState,useRef} from "react";
+import styles from "../Navbar/navbar.module.css";
+import IconButton from "@mui/material/IconButton";
+import PersonIcon from "@mui/icons-material/Person";
 
-import Paper from '@mui/material/Paper';
+import Paper from "@mui/material/Paper";
 
+import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
 
+import ZoomOutMapRoundedIcon from "@mui/icons-material/ZoomOutMapRounded";
+import Divider from "@mui/material/Divider";
+import InsertPhotoOutlinedIcon from "@mui/icons-material/InsertPhotoOutlined";
+import EmojiEmotionsOutlinedIcon from "@mui/icons-material/EmojiEmotionsOutlined";
+import PollOutlinedIcon from "@mui/icons-material/PollOutlined";
+import PublicOutlinedIcon from "@mui/icons-material/PublicOutlined";
 
-import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
+import WarningIcon from "@mui/icons-material/Warning";
 
-import ZoomOutMapRoundedIcon from '@mui/icons-material/ZoomOutMapRounded';
-import Divider from '@mui/material/Divider';
-import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
-import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
-import PollOutlinedIcon from '@mui/icons-material/PollOutlined';
-import PublicOutlinedIcon from '@mui/icons-material/PublicOutlined';
-
-
-import WarningIcon from '@mui/icons-material/Warning';
-
-import TimerIcon from '@mui/icons-material/Timer';
+import TimerIcon from "@mui/icons-material/Timer";
+import MediaCard from "./FirstPost";
 
 const Postpage = () => {
-    return (
-        <div>
-            
-            
- 
- 
-  <div  className={styles.middlemenubar}>
-    <Paper elevation={2} className={styles.middlemenubar1}>
-     <div className={styles.middlemenubar2}>
-<div className={styles.samecolor2}>
-<IconButton  style={{border:"50%",background:"rgb(240,240,240)"}}
-size="medium">
 
+  const input1=useRef(null)
+  const addfun=()=>{
+    if(input1.current.style.display==="none"){
+      input1.current.style.display="block"
+    }
+    else{
+      input1.current.style.display="none"
 
-  <PersonIcon  />
-  </IconButton>
-</div>
-
-<div className={styles.postdiv}>
-<p>   Post to timeline
-</p>
-<ArrowDropDownRoundedIcon style={{marginTop:"1px"}} />
-
-</div>
-
-<div className={styles.zoom} >
-<ZoomOutMapRoundedIcon style={{color:"gray"}} />
-
-</div>
-</div>
-<Divider light />
-
-
-<div  className={styles.postinputfieeld}>
-    
-<input  className={styles.inputtextfield}
-        // hiddenLabel
-        // id="filled-hidden-label-normal"
+    }
+  }
   
-        placeholder="What's on your mind?"
-        // variant="filled"
-      />
-  </div>
+  return (
+    <div>
+      <div className={styles.middlemenubar}>
+        <Paper elevation={2} className={styles.middlemenubar1}>
+          <div className={styles.middlemenubar2}>
+            <div className={styles.samecolor2}>
+              <IconButton
+                style={{ border: "50%", background: "rgb(240,240,240)" }}
+                size="medium"
+              >
+                <PersonIcon />
+              </IconButton>
+            </div>
 
-  <Paper  elevation={3} className={styles.postitems}>
-    <InsertPhotoOutlinedIcon className={styles.emojis} style={{color:"#63da9d" ,fontSize:"25px"}} />
+            <div className={styles.postdiv}>
+              <p> Post to timeline</p>
+              <ArrowDropDownRoundedIcon style={{ marginTop: "1px" }} />
+            </div>
 
+            <div className={styles.zoom}>
+              <ZoomOutMapRoundedIcon style={{ color: "gray" }} />
+            </div>
+          </div>
+          <Divider light />
+
+
+          <div ref={input1} className={styles.postinputfieeld}>
+            
+            <input
+              className={styles.inputtextfield}
+             onFocus={addfun}
+              type="text"
+              
+              
+              // onChange={handleSearch}
+
+              placeholder="What's on your mind?"
+          />
+
+
+
+
+
+
+          </div>
+
+          <Paper elevation={3} className={styles.postitems}>
+            {/* <InsertPhotoOutlinedIcon  className={styles.emojis} style={{color:"#63da9d" ,fontSize:"25px"}} />
 
     <EmojiEmotionsOutlinedIcon   className={styles.emojis1}  style={{color:"#f6b83c" ,fontSize:"20px"}}/>
  
-
  <PollOutlinedIcon className={styles.emojis} style={{color:"#f87e3a" }}/>
   <PublicOutlinedIcon className={styles.emojis} style={{color:"#35bba7" }}/>
   <WarningIcon  className={styles.emojis} style={{color:"#8c75c9" }}/>
 
 <i  style={{color:"red",marginTop:"12px",margin:"10px"}} class="fas fa-calendar-alt" ></i>
-<TimerIcon className={styles.emojis} style={{color:"#ee2c4d" }}/>
-  </Paper>
+<TimerIcon className={styles.emojis} style={{color:"#ee2c4d" }}/> */}
 
-    </Paper>
-  
+            <div className={styles.iconsupdated}>
+              <p className={styles.iconsinput}>
+                <InsertPhotoOutlinedIcon for="id1" className={styles.iconsinputinsert} />
+                <label htmlFor="">
+
+               <input id="id1" style={{backgroundColor:"white"}} className={styles.fileinput} type="file" />
+
+                </label>
+
+              </p>
+            </div>
+          </Paper>
 
 
+<div ref={input1}>
+  <button className={styles.btnpost}>Post</button>
+</div>
 
-  </div>
+        </Paper>
+        <div style={{ marginTop: "20px" }}>
+          <MediaCard />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Postpage
+export default Postpage;
