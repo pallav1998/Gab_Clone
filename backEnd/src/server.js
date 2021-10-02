063f93a4678c668fb9fc0e90bd75a13fd2439c28
NOOR <noormuhammed14@gmail.com>
2021-10-02 15:25:00 +0530

    frontend upload

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -2,22 +2,21 @@ const express=require("express");
 const connect=require("./config/db");
 const dotenv = require("dotenv");
 const cors=require("cors")
-
 const app = express();
-app.use(cors())
+
 app.use(express.json());
 const userSchema=require("./controller/user.controller")
 const logingRouter=require("./controller/login.controller")
 
 
 
-
+app.use(cors());
 
 app.use("/users",userSchema);
 app.use("/login",logingRouter);
 
 
-app.listen(4000,async()=>{
+app.listen(8000,async()=>{
 await connect();
 console.log("Listening on port 4000")
 })
