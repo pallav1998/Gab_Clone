a12d4da635198586941e6fe1180afffd688ca4de
NOOR <noormuhammed14@gmail.com>
2021-10-04 18:14:17 +0530

    some changes

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+const UserSchema = new mongoose.Schema(
+  {
+    // first_name: { type: String, required: true },
+    // last_name: { type: String, required: true },
+    user_name: { type: String, required: true },
+    password: { type: String, required: true },
+    confirm_password: { type: String, required: true },
+    gender: { type: String, required: false },
+    age: { type: Number, required: false },
+    email: { type: String, required: true },
+    photo_url: [{ type: String, required: false }],
+    tokens: [{ token: { type: String, required: false } }],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.confirm_password = await bcrypt.hash(this.confirm_password, 12);
+  }
+  next();
+});
+
+UserSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
