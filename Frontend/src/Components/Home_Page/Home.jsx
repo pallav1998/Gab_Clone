267169c3afc18834462a27751fcff5aff1d65513
NOOR <noormuhammed14@gmail.com>
2021-10-04 16:36:41 +0530

    homepage edit

--- Frontend/src/Components/Home_Page/Home.jsx
+++ Frontend/src/Components/Home_Page/Home.jsx
@@ -1,27 +1,111 @@
-import React from "react";
-import "./home.css";
-
-
+import React from 'react'
+import "./home.css"
 const Home = () => {
-  return (
-    <div className="container">
-      <div className="left">
-        <h2>Gab.com</h2>
-        <p>menu</p>
-        <i class="fas fa-home">Home</i>
-        <i class="fas fa-search">Search</i>
-        <i class="fas fa-users">Group</i>
-        <i class="fas fa-file-alt">News</i>
-        <i class="far fa-address-card">About</i>
-        <p>Explore</p>
-        <i class="fas fa-laptop-code">Apps</i>
-        <i class="fas fa-tags">Shop</i>
-        <i class="fas fa-poll-h">Trend</i>
-      </div>
-      <div className="between"></div>
-      <div className="right"></div>
-    </div>
-  );
-};
+    return (
+        <div className="homeContainer">
+            <div className="left1">
+                <h2>Gab.com</h2>
+                <p>menu</p>
+                <i class="fas fa-home">  Home </i>
+                <i class="fas fa-search">  Search</i>
+                <i class="fas fa-users">   Group</i>
+                <i class="fas fa-file-alt">  News</i>
+                <i class="far fa-address-card"> About</i>
+                <p style={{marginTop:"20px"}}>Explore</p>
+                <i class="fas fa-laptop-code"> Apps</i>
+                <i class="fas fa-tags">  Shop</i>
+                <i class="fas fa-poll-h">  Trend</i>
+            </div>
+            <div className="between">
+                <div className="post1">
+                      
+                <div className="name3">
+                 <div> <img src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png" alt="loading" height="50px" /></div>
+                 <div style={{display:"flex"}}>
+                  <h2>Noor</h2>
+                 <p>@noormuhammed</p>
+                 </div>
+                  </div>
 
-export default Home;
+                   <img src="https://image.shutterstock.com/image-photo/hacker-virus-malware-attack-during-260nw-1704713953.jpg" alt="loading" />
+                
+                  <div className="like"><button><i class="far fa-thumbs-up"></i>like</button>
+                  <button><i class="far fa-comment-alt"></i>Comments</button>
+                  <button><i class="fas fa-redo"></i>Repost</button>
+                  <button><i class="far fa-share-square"></i>share</button>
+                 <button><i class="fas fa-quote-right"></i>Qutos</button>
+                  </div>
+</div>
+ <div className="post1">
+                   <div className="name3">
+                 <div> <img src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png" alt="loading" height="50px" /></div>
+                 <div style={{display:"flex"}}>
+                  <h2>Noor</h2>
+                 <p>@noormuhammed</p>
+                 </div>
+                  </div>
+
+                   <img src="https://image.shutterstock.com/image-photo/hacker-virus-malware-attack-during-260nw-1704713953.jpg" alt="loading" />
+                
+                  <div className="like"><button><i class="far fa-thumbs-up"></i>like</button>
+                  <button><i class="far fa-comment-alt"></i>Comments</button>
+                  <button><i class="fas fa-redo"></i>Repost</button>
+                  <button><i class="far fa-share-square"></i>share</button>
+                 <button><i class="fas fa-quote-right"></i>Qutos</button>
+                  </div>
+                     </div>
+ <div className="post1">
+
+                   <div className="name3">
+                 <div> <img src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png" alt="loading" height="50px" /></div>
+                 <div style={{display:"flex"}}>
+                  <h2>Noor</h2>
+                 <p>@noormuhammed</p>
+                 </div>
+                  </div>
+
+                   <img src="https://image.shutterstock.com/image-photo/hacker-virus-malware-attack-during-260nw-1704713953.jpg" alt="loading" />
+                
+                  <div className="like"><button><i class="far fa-thumbs-up"></i>like</button>
+                  <button><i class="far fa-comment-alt"></i>Comments</button>
+                  <button><i class="fas fa-redo"></i>Repost</button>
+                  <button><i class="far fa-share-square"></i>share</button>
+                 <button><i class="fas fa-quote-right"></i>Qutos</button>
+                  </div>
+                  </div>
+<div className="post1">
+                   <div className="name3">
+                 <div> <img src="https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png" alt="loading" height="50px" /></div>
+                 <div style={{display:"flex"}}>
+                  <h2>Noor</h2>
+                 <p>@noormuhammed</p>
+                 </div>
+                  </div>
+                 
+                   {/* <img src="https://image.shutterstock.com/image-photo/hacker-virus-malware-attack-during-260nw-1704713953.jpg" alt="loading" /> */}
+                
+      
+                  <div className="like"><button><i class="far fa-thumbs-up"></i>like</button>
+                  <button><i class="far fa-comment-alt"></i>Comments</button>
+                  <button><i class="fas fa-redo"></i>Repost</button>
+                  <button><i class="far fa-share-square"></i>share</button>
+                 <button><i class="fas fa-quote-right"></i>Qutos</button>
+                  </div>
+              </div>
+              
+            </div>
+            <div className="right">
+              <div className="rightPost">
+                   
+              
+                 </div>
+                
+        
+              
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
